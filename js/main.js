bd14cfc16674412dc8a08a450a5436b83c43c751
Max Juchheim <maxjuchheim@gmail.com>
2017-12-28 18:49:19 -0500

    finished game logic with DOM (MVP for assignment)

--- js/main.js
+++ js/main.js
@@ -29,12 +29,27 @@ var checkforMatch = function() {
 		}
 	}
 }
-var flipCard = function(cardId) {
+var flipCard = function() {
+	if (cardsInPlay.length ===2 ) {
+		return;
+	}
+	var cardId = this.getAttribute("data-id");
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute("src", cards[cardId].cardImage);
 	checkforMatch();
 }
-flipCard(0);
-flipCard(2);
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+
+createBoard();
