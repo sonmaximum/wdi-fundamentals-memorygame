ec83d38db0e188c751cf82ceb5e5c0915e97d326
Max Juchheim <maxjuchheim@gmail.com>
2017-12-27 22:24:46 -0500

    add fuctions to organize code

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped "+cardsInPlay[0]);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped "+cardsInPlay[1]);
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
+var checkforMatch = function() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
 	}
 }
+var flipCard = function(cardId) {
+	console.log("User flipped "+cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkforMatch();
+}
+flipCard(0);
+flipCard(2);
