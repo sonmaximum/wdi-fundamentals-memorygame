bc4a28b1b9fc3f5aa9135dcd98f593eb0a955bf1
Max Juchheim <maxjuchheim@gmail.com>
2017-12-28 20:44:43 -0500

    added some of the bonus challenge stuff to the deliverable

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+//bonus addition, function to randomize cards before loading board
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
 var cards = [
 {
 rank: "queen",
@@ -19,15 +40,31 @@ rank: "king",
 suit: "diamonds",
 cardImage: "images/king-of-diamonds.png"}
 ];
+
+cards = shuffle(cards);
+
 var cardsInPlay = [];
-var checkforMatch = function() {
+var checkForMatch = function() {
 	if (cardsInPlay.length === 2) {
+		tries++;
+		document.getElementById("tries").textContent = tries;
 		if (cardsInPlay[0] === cardsInPlay[1]) {
+			matches++
+			document.getElementById("matches").textContent = matches;
 			alert("You found a match!");
 		} else {
 			alert("Sorry, try again.");
 		}
 	}
+}
+
+var tries = 0
+var matches = 0
+
+/* Give the browser time to redraw the flipped card 
+before showing the check for match alert */
+var checkForMatchWait = function(){
+	setTimeout(checkForMatch, 50);
 }
 var flipCard = function() {
 	if (cardsInPlay.length ===2 ) {
@@ -39,7 +76,7 @@ var flipCard = function() {
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	this.setAttribute("src", cards[cardId].cardImage);
-	checkforMatch();
+	checkForMatchWait();
 }
 
 var createBoard = function() {
@@ -51,5 +88,15 @@ var createBoard = function() {
 		document.getElementById("game-board").appendChild(cardElement);
 	}
 }
+
+var resetBoard = function(){
+	cardsInPlay = [];
+	var cardImgs = document.querySelectorAll("img");
+	for (var i = 0; i < cardImgs.length; i++) {
+		cardImgs[i].setAttribute("src", "images/back.png");
+	}
+}
+
+document.querySelector("button").addEventListener("click", resetBoard);
 
 createBoard();
